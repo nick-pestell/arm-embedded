b0a9708200e8040f89955f3ceaee3884dc2c4bba
nick-pestell <np0877@bristol.ac.uk>
2021-03-28 22:08:27 +0100

    blinking LED working

--- lesson_3/src/main.c
+++ lesson_3/src/main.c
@@ -2,39 +2,52 @@
 #include <stdio.h>
 
 int main(void) {
-	// Enable GPIOA and GPIOB peripheral in RCC_AHB1ENR.
+	// Enable GPIOC and GPIOB peripheral in RCC_AHB1ENR.
 	// GPIOA and B are on bus AHB1 (see pg57 of refference manual)
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; //RCC is a pointer to a structure. RCC->member is specific syntac for defrencing (getting the value) of the member of the structure that RCC points to. (see k&r pg131)
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN; //RCC is a pointer to a structure. RCC->member is specific syntac for defrencing (getting the value) of the member of the structure that RCC points to. (see k&r pg131)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-	// Configure button GPIO for input
-	GPIOC->MODER &= ~(0x3 << (BUTTON_PIN*2));// set the bits that represent button pin (2*button_pin and 2*button_pin +1) to 00
+	// Configure button input GPIO for input
+	GPIOE->MODER &= ~(0x3 << (BUTTON_PIN_INPUT*2));// set the bits that represent button pin (2*button_pin and 2*button_pin +1) to 00
 
-	// Configure button GPIO with pull up resistor
-	GPIOC->PUPDR &= ~(0x3 << (BUTTON_PIN*2));
-	GPIOC->PUPDR |= ~(0x2 << (BUTTON_PIN*2));
+	// Configure button input GPIO with pull down resistor
+	GPIOE->PUPDR &= ~(0x3 << (BUTTON_PIN_INPUT*2));
+	GPIOE->PUPDR |= ~(0x1 << (BUTTON_PIN_INPUT*2));
+
+
+
+	// Configure button power GPIO for output
+	GPIOA->MODER &= ~(0x3 << (BUTTON_PIN_POWER*2));
+	GPIOA->MODER |= (0x1 << (BUTTON_PIN_POWER*2));
+
+	// Configure button power GPIO as push/pull type
+	GPIOA->OTYPER &= ~(1 << (BUTTON_PIN_POWER)); //set the bit that represents led1 to 0 for push/pull type
+
+	// Pull button power GPIO pin high by setting the relevant bit in the output data register to high (1)
+	GPIOA->ODR |= (1 << BUTTON_PIN_POWER);
+
 
 	
 	// Configure led1 GPIO for output
 	GPIOB->MODER &= ~(0x3 << (LED_PIN*2));
-	GPIOB->MODER |= ~(0x2 << (LED_PIN*2));// set the bits that represent the led_pin (2*led_pin and 2*led_pin+1) to 01. To represent 01, i.e. 2 bits we do ~0x2, 0x2 is 10 in binary.
+	GPIOB->MODER |=  (0x1 << (LED_PIN*2));// set the bits that represent the led_pin (2*led_pin and 2*led_pin+1) to 01. To represent 01, i.e. 2 bits we do ~0x2, 0x2 is 10 in binary.
 
 	// Configure led1 GPIO as push/pull type
-	GPIOB->OTYPER &= (0 << (LED_PIN*2)); //set the bit that represents led1 to 0 for push/pull type
-
+	GPIOB->OTYPER &= ~(1 << (LED_PIN)); //set the bit that represents led1 to 0 for push/pull type
 
 	// Pull LED1 pin low by setting the relevant bit in the output data register to high (1)
-	GPIOB->ODR |= (1 << LED_PIN);
+	GPIOB->ODR |= (0 << LED_PIN);
 
 	uint8_t button_down = 0;
 	uint32_t gpioa_input;
 	while(1){
-		// Read status of each GPIOA pin by reading GPIOA peripherals input data register
+		// Read status of each GPIOC pin by reading GPIOC peripherals input data register
 		// gpioa_input = GPIOA->IDR;
-		gpioa_input = ~GPIOC->IDR;
+		gpioa_input = GPIOE->IDR;
 		// check if button is down
 		// if ((gpioa_input | (0 << BUTTON_PIN)) == 0){
-		if (gpioa_input & (1 << BUTTON_PIN)) {
+		if (gpioa_input & (1 << BUTTON_PIN_INPUT)) {
 			if (!button_down){
 				GPIOB->ODR ^= (1 << LED_PIN); //turn on/off LED1
 			}
--- lesson_3/src/main.h
+++ lesson_3/src/main.h
@@ -4,10 +4,13 @@
 #include <stdint.h>
 #include "stm32f722xx.h"
 
+// Define GPIOE pin mappings for our button.
+#define BUTTON_PIN_INPUT (0) // using PE0
+
 // Define GPIOA pin mappings for our button.
-#define BUTTON_PIN (13)
+#define BUTTON_PIN_POWER (0) // using PA0 to power button
 
 // Define GPIOB pin mappings for out LED.
-#define LED_PIN    (0)
+#define LED_PIN    (0) //user led is on PB0
 
 #endif
